91f4cda7b73fb1c04961b03bcb7054ed94d9519b
Takats Balint <takats.balint1@gmail.com>
2023-03-21 12:15:52 +0100

    bugfixes

--- cleaning.py
+++ cleaning.py
@@ -4,22 +4,25 @@ from tqdm import tqdm
 import os
 
 
-WINDOW = 10000
-paths = os.listdir('./data')
+WINDOW = 5000
+paths = os.listdir('./duped')
 paths = [path for path in paths if path.endswith('.txt')]
 
 def remove_near_duplicates(strings):
     # Create an empty list to store unique strings
     unique_strings = []
-    duplicates = []
+    # duplicates = {}
     
     # Loop through each string in the input list
-    for string in strings:
+    for string in tqdm(strings):
         # Check if the string is similar to any existing unique strings
-        is_duplicate = False
+        
         if string == '\n':
             unique_strings.append(string)
             continue
+        # if string in duplicates:
+        #     continue
+        is_duplicate = False
         for unique_string in unique_strings:
             # Calculate the Levenshtein distance between the strings
             lev_distance = distance(string, unique_string)
@@ -36,19 +39,27 @@ def remove_near_duplicates(strings):
         # If the string is not a near duplicate, add it to the list of unique strings
         if not is_duplicate:
             unique_strings.append(string)
-        else:
-            duplicates.append(string)
+        # else:
+        #     duplicates[string] = True
+        #     # duplicates.append(string)
     
     # Return the list of unique strings
-    return unique_strings, duplicates
+    return unique_strings
 
 
-for i in paths:
-    with open('./data/'+i, encoding="utf-8") as f:
+import multiprocessing
+
+def remove_duplicates(path):
+    with open('./duped/'+path, encoding="utf-8") as f:
         data = f.readlines()
     unduped = []
     for i in range(0, len(data),WINDOW ):
-        unique, dupes = remove_near_duplicates(data[i:i+WINDOW])
+        unique = remove_near_duplicates(data[i:i+WINDOW])
+        print("done")
         unduped.extend(unique)
-    with open('./dedup-data/'+i, encoding="utf-8") as f:
-        f.write(unduped)
+    with open('./dedup-data/'+path, encoding="utf-8", mode="w") as f:
+        f.write(''.join(unduped))
+
+if __name__ == '__main__':
+    with multiprocessing.Pool() as pool:
+        pool.map(remove_duplicates, paths)
